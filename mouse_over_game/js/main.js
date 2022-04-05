77582c12ce2cea48c21315b23a631ee76e75ae76
Paula Builes <pbuiles1026@cue.edu.co>
2022-04-05 15:14:53 -0500

    Juego rediseñado

--- mouse_over_game/js/main.js
+++ mouse_over_game/js/main.js
@@ -1,29 +1,32 @@
 document.getElementById('player').addEventListener("mouseover",sumarPuntos);
 
 var puntos=0;
-var tiempo=30;
-var puntosObjetivo=30;
+var tiempo=50;
+var puntosObjetivo=50;
 
 function sumarPuntos(){
  puntos++;
  document.getElementById("score").innerHTML= "PUNTOS: <b>"+puntos+ "/"+puntosObjetivo+"</b>";
- let ranNum= Math.round(Math.random()*500);
+ let ranNum= Math.round(Math.random()*500); //esta linea de codigo funciona para que cuanod el usuario pase por en cima de la bolita morado el raton del mouse, esta se mueva y cambie de posicion.
  let ranNum2= Math.round(Math.random()*500);
  document.getElementById("player").style.marginTop=ranNum+"px";
  document.getElementById("player").style.marginLeft=ranNum+"px";
- if(puntos==30){
-     alert("ganaste");
+ if(puntos==50){
+     alert("Felicidades ganaste el juego");
  }
 }
 
 function restarTiempo(){
     tiempo--;
+    document.getElementById("score").innerHTML= "PUNTOS: <b>"+puntos+ "/"+puntosObjetivo+"</b>";
     document.getElementById("time").innerHTML=" Tiempo: "+tiempo+ " ";
     if(tiempo==0){
-        alert("Perdiste!");
-        tiempo=30;
+        alert("¡Perdiste el juego!");
+        tiempo=50;
         puntos=0;
     }
 }
 
-setTimeout(restarTiempo,500);
+setInterval(restarTiempo,500);
+
+
